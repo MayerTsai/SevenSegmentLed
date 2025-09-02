f3500669561dafd0fb785b9742341804248b195d
Mayer <mayer.tsai@gmail.com>
2025-09-02 19:28:01 +0800

    add: disable the copy constructor

--- lib/src/SevenSegmentLedDriver.h
+++ lib/src/SevenSegmentLedDriver.h
@@ -8,13 +8,15 @@ class seven_segment_led_driver
 private:
   SevenSegmentLed *_led;
   unsigned long _blinking_duration;
-  
+
   unsigned long _last_blink_time;
   bool _is_on;
   int _last_number;
 
 public:
   seven_segment_led_driver(SevenSegmentLed &led, unsigned long duration = 1000);
+  seven_segment_led_driver(const seven_segment_led_driver &) = delete;
+  seven_segment_led_driver &operator=(const seven_segment_led_driver &) = delete;
   ~seven_segment_led_driver() = default;
 
   void set_blinking_duration(unsigned long duration);
